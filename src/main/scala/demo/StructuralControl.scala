6bbc2a00f56bc4a14dd8c65fc68a6f5f5afe2412
yehui.mao <maoyehui94@gmail.com>
2018-03-24 18:29:39 +0800

    update Object StructureControl

--- src/main/scala/demo/StructuralControl.scala
+++ src/main/scala/demo/StructuralControl.scala
@@ -11,4 +11,46 @@ package demo
   */
 object StructuralControl {
 
+
+  def main(args: Array[String]): Unit = {
+
+    // for循环
+    val a = Array("apple", "banana", "orange")
+    for (e <- a) println(e)
+    for (e <- a) {
+      val s = e.toUpperCase()
+      println(s)
+    }
+
+    // 从for循环中返回值
+    val newArray = for (e <- a) yield {
+      val s = e.toUpperCase()
+      s
+    }
+    println(newArray.toList.toString)
+
+    // for循环计数器
+    for (i <- 0 until a.length) {
+      println(s"$i is ${a(i)}")
+    }
+    for (i <- 1 to 2; j <- 1 to 2) println(s"i = $i, j = $j")
+
+    // 卫语句
+    for (i <- 1 to 10 if i < 4) println(i)
+
+    // for推导不会改变集合的返回类型
+    var fruits = scala.collection.mutable.ArrayBuffer[String]()
+    fruits += "apple"
+    fruits += "banana"
+    fruits += "orange"
+    println(fruits.asInstanceOf[AnyRef].getClass.getSimpleName)
+    val out = for (e <- fruits) yield e.capitalize
+    println(out.asInstanceOf[AnyRef].getClass.getSimpleName)
+
+    val fruit = "apple" :: "banana" :: "orange" :: Nil
+    val o = for (e <- fruit) yield e.toUpperCase()
+
+    // 调用map方法可以起到相同的效果(map等价于for/yeild等)
+    val result = fruit.map(_.toUpperCase())
+  }
 }
