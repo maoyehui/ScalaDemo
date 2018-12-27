bb53b23e2c817b002297697a52c111bb674f2b0b
yehui.mao <maoyehui94@gmail.com>
2018-12-27 20:21:35 +0800

    update .gitignore

--- src/main/scala/demo/spark/PageRankDemo.scala
+++ src/main/scala/demo/spark/PageRankDemo.scala
@@ -1,14 +1,39 @@
 package demo.spark
 
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
+
 /**
   * @ProjectName: SBTTest
   * @Package: demo.spark
   * @ClassName: PageRankDemo
-  * @Description:
+  * @Description: PageRank算法
   * @Author: yehui.mao
   * @CreateDate: 2018/12/27 15:00
   * @UpdateUser: yehui.mao
   */
-object PageRankDemo {
+object PageRankDemo extends App {
+  val conf = new SparkConf()
+    .setMaster("local[*]")
+    .setAppName("PageRank Demo")
+  val sc = new SparkContext(conf)
 
+  val linkRoute = Seq("baidu sina google", "sina taobao jd", "google jd", "jd taobao")
+
+  val links = sc.parallelize(linkRoute)
+    .map(l => (l.split(" ")(0), l.split(" ")))
+    .partitionBy(new HashPartitioner(2))
+    .persist()
+
+  var ranks = links.mapValues(v => 1.0)
+
+  for (i <- 0 until 10) {
+    var contributions = links.join(ranks).flatMap {
+        case (pageId, (link, rank)) =>
+          link.map(dest => (dest, rank / link.size))
+      }
+    ranks = contributions.reduceByKey((x, y) => x + y)
+      .mapValues(v => 0.15 + 0.85 * v)
+  }
+
+  ranks.foreach(println(_))
 }
