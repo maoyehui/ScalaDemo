3986f0e1dc6a3313561c4f02cd1458bc28dc4e34
yehui.mao <maoyehui94@gmail.com>
2018-12-27 20:19:45 +0800

    修改gitignore

---
+++ src/main/scala/demo/spark/PageRankDemo.scala
@@ -0,0 +1,14 @@
+package demo.spark
+
+/**
+  * @ProjectName: SBTTest
+  * @Package: demo.spark
+  * @ClassName: PageRankDemo
+  * @Description:
+  * @Author: yehui.mao
+  * @CreateDate: 2018/12/27 15:00
+  * @UpdateUser: yehui.mao
+  */
+object PageRankDemo {
+
+}
