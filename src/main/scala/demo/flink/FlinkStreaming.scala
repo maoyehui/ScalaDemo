672e2a4c7cc8e75b8ee91c2c31c22d15ad9af4f0
yehui.mao <maoyehui94@gmail.com>
2019-04-08 16:26:28 +0800

    add flink dependences

---
+++ src/main/scala/demo/flink/FlinkStreaming.scala
@@ -0,0 +1,14 @@
+package demo.flink
+
+/**
+  * @ProjectName: SBTTest
+  * @Package: demo.flink
+  * @ClassName: FlinkStreaming
+  * @Description:
+  * @Author: yehui.mao
+  * @CreateDate: 2019/4/8 16:23
+  * @UpdateUser: yehui.mao
+  */
+object FlinkStreaming {
+
+}
